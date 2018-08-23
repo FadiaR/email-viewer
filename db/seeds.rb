11aa0b1bc66f2e1c2405fc3cb2903e135aa565d3
Fadia Rostane <39888576+FadiaR@users.noreply.github.com>
2018-08-23 19:18:27 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  
-Email.create(object: 'Les pommes', 
-						 body: 'This is the 1st seed email... I can say alotttt of bs but I\'ll just stop right here :)') 
+
+Email.create(object: 'Les pommes',
+						 body: 'This is the 1st seed email... I can say alotttt of bs but I\'ll just stop right here :)')
 
 Email.create(object: 'Maison',
 						 body: 'Hello chou, je devrais etre de retour a la maison le 15 au soir :) je te dis ç maintenant histoire que tu ai le temps de tout ranger ;) bisous')
@@ -22,7 +22,7 @@ Email.create(object: '30 recettes avec des pâtes!',
 						 body: 'T\'as pas assez de thune pour manger autre choses que des pâtes ? TKT nous on a LA solution et on te propose une liste non exhaustive de 30 super recettes aves des pâtes \\o/ le lien ci dessous [redacted] ')
 
 Email.create(object: 'Nouveau : Les Sims Au Cimetière !',
-						 body: 'ENFIN le nouveau DLC des Sims est sorti ! Emmène tes sims au cimetiere fais les sauter sur des tombes pêcher dans le lac des âmes ou se faire couper la tête par la Grande Faucheuse !!! Et en ce moment, prix mini ! Achète-le pour seulement 80$ avec le code [redacted] !!') 
+						 body: 'ENFIN le nouveau DLC des Sims est sorti ! Emmène tes sims au cimetiere fais les sauter sur des tombes pêcher dans le lac des âmes ou se faire couper la tête par la Grande Faucheuse !!! Et en ce moment, prix mini ! Achète-le pour seulement 80$ avec le code [redacted] !!')
 
 Email.create(object: 'Pick-up',
 						 body: 'This is the 2nd seed email... huhuhu hahaha trololo')
@@ -39,14 +39,11 @@ Email.create(object: 'Adopte un chaton :3',
 Email.create(object: 'Fais pas cette tête',
 						 body: 'C\'est bon fais pas la gueule c\'était juste pour rire :/ je pensais pas que c\'était indélébile ') 
 
-Email.create(object: 'PUBG obnubile ton mec ? ...',
-						 body: 'Bonne nouvelle le mien aussi il lâche pas u.u je comprends XD')
-
 Email.create(object: 'Artemis Spectrum ! Le casque logitech qui te met bien...',
 						 body: 'Cher mais styley *q*    https://www.logitechg.fr/fr-fr/product/g633-7-1-surround-sound-gaming-headset        #nonjefaispasdepub :p ')
 
 Email.create(object: 'Techniques pour repasser une chemise',
 						 body: 'May 4th .... B w/ u XD')
 
-Email.create(object: 'J\'ai tes tongs dans mon sac', 
+Email.create(object: 'J\'ai tes tongs dans mon sac',
 						body: 'Heuuu c\'est normal qu\'il y a tes tongs dans mon sac ?')
